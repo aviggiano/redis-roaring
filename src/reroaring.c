422e228d3f7832e3e169240798360d1bd60412c2
Antonio Viggiano <agfviggiano@gmail.com>
2017-05-09 21:15:47 -0300

    Create type.[ch] files to isolate redis module type code

--- src/reroaring.c
+++ src/reroaring.c
@@ -1,61 +1,8 @@
 #include "redismodule.h"
 #include "data_structure.h"
-
-#define BITMAP_ENCODING_VERSION 1
+#include "type.h"
 
 static RedisModuleType* BitmapType;
-
-/* === Bitmap type methods === */
-void BitmapRdbSave(RedisModuleIO* rdb, void* value) {
-  Bitmap* bitmap = value;
-  size_t serialized_size = roaring_bitmap_size_in_bytes(bitmap);
-  char* serialized_bitmap = RedisModule_Alloc(serialized_size);
-  size_t serialized_size_check = roaring_bitmap_serialize(bitmap, serialized_bitmap);
-
-  RedisModule_SaveStringBuffer(rdb, serialized_bitmap, serialized_size_check);
-
-  RedisModule_Free(serialized_bitmap);
-}
-
-void* BitmapRdbLoad(RedisModuleIO* rdb, int encver) {
-  if (encver != BITMAP_ENCODING_VERSION) {
-    RedisModule_LogIOError(rdb, "warning", "Can't load data with version %d", encver);
-    return NULL;
-  }
-  size_t size;
-  char* serialized_bitmap = RedisModule_LoadStringBuffer(rdb, &size);
-  Bitmap* bitmap = roaring_bitmap_deserialize(serialized_bitmap);
-  return bitmap;
-}
-
-typedef struct Bitmap_aof_rewrite_callback_params_s {
-  RedisModuleIO* aof;
-  RedisModuleString* key;
-} Bitmap_aof_rewrite_callback_params;
-
-bool BitmapAofRewriteCallback(uint32_t offset, void* param) {
-  Bitmap_aof_rewrite_callback_params* params = param;
-  RedisModule_EmitAOF(params->aof, "R.SETBIT", "sll", params->key, offset, 1);
-  return true;
-}
-
-void BitmapAofRewrite(RedisModuleIO* aof, RedisModuleString* key, void* value) {
-  Bitmap* bitmap = value;
-  Bitmap_aof_rewrite_callback_params params = {
-      aof: aof,
-      key: key
-  };
-  roaring_iterate(bitmap, BitmapAofRewriteCallback, &params);
-}
-
-size_t BitmapMemUsage(const void* value) {
-  const Bitmap* bitmap = value;
-  return roaring_bitmap_size_in_bytes(bitmap);
-}
-
-void BitmapFree(void* value) {
-  bitmap_free(value);
-}
 
 /* === Bitmap type commands === */
 
---
+++ src/type.h
@@ -0,0 +1,10 @@
+#ifndef REROARING_TYPE_H
+#define REROARING_TYPE_H
+
+void BitmapRdbSave(RedisModuleIO* rdb, void* value);
+void* BitmapRdbLoad(RedisModuleIO* rdb, int encver);
+void BitmapAofRewrite(RedisModuleIO* aof, RedisModuleString* key, void* value);
+size_t BitmapMemUsage(const void* value);
+void BitmapFree(void* value);
+
+#endif
