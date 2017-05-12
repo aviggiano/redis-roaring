6e73d2fd64de12a0ea969a79655f9ef7715365e8
Antonio Viggiano <agfviggiano@gmail.com>
2017-05-11 22:15:30 -0300

    Add integration test for wrong arity

--- tests/integration.sh
+++ tests/integration.sh
@@ -6,11 +6,21 @@ function test_rsetbit_rgetbit()
 {
   echo "test_rsetbit_rgetbit"
   EXPECTED=0
-  FOUND=$(echo "R.SETBIT key 0 1" | redis-cli)
+  FOUND=$(echo "R.SETBIT test_rsetbit_rgetbit 0 1" | redis-cli)
   [ $FOUND -eq $EXPECTED ]
   EXPECTED=1
-  FOUND=$(echo "R.GETBIT key 0" | redis-cli)
+  FOUND=$(echo "R.GETBIT test_rsetbit_rgetbit 0" | redis-cli)
   [ $FOUND -eq $EXPECTED ]
+}
+function test_rsetbit_rgetbit_wrong_arity()
+{
+  echo "test_rsetbit_rgetbit_wrong_arity"
+  EXPECTED="ERR wrong number of arguments for 'R.SETBIT' command"
+  FOUND=$(echo "R.SETBIT test_rsetbit_rgetbit_wrong_arity 0" | redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+  EXPECTED="ERR wrong number of arguments for 'R.GETBIT' command"
+  FOUND=$(echo "R.GETBIT key 0 1" | redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
 }
 function test_done()
 {
@@ -18,4 +28,5 @@ function test_done()
 }
 
 test_rsetbit_rgetbit
+test_rsetbit_rgetbit_wrong_arity
 test_done
