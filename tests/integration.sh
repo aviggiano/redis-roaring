b73bbaca20f5b2619592a73f35f538eefa4711d6
Antonio Viggiano <agfviggiano@gmail.com>
2017-05-12 00:10:38 -0300

    Add tests to functions

--- tests/integration.sh
+++ tests/integration.sh
@@ -25,22 +25,78 @@ function test_rsetbit_rgetbit_wrong_arity()
 function test_rbitop()
 {
   echo "test_rbitop"
-  EXPECTED="ERR syntax error"
-  FOUND=$(echo "R.BITOP test_rsetbit_rgetbit_wrong_arity NOOP srckey1 srckey2" | redis-cli)
-  [ "$FOUND" == "$EXPECTED" ]
-  FOUND=$(echo "R.SETBIT test_rsetbit_rgetbit_wrong_arity_4 1 1" | redis-cli)
-  FOUND=$(echo "R.SETBIT test_rsetbit_rgetbit_wrong_arity_4 2 1" | redis-cli)
-  FOUND=$(echo "R.SETBIT test_rsetbit_rgetbit_wrong_arity_2 1 1" | redis-cli)
-  FOUND=$(echo "R.BITOP AND test_rsetbit_rgetbit_wrong_arity_dest_2 test_rsetbit_rgetbit_wrong_arity_2 test_rsetbit_rgetbit_wrong_arity_4" | redis-cli)
-  EXPECTED="1"
-  [ "$FOUND" == "$EXPECTED" ]
-  FOUND=$(echo "R.BITOP OR test_rsetbit_rgetbit_wrong_arity_dest_6 test_rsetbit_rgetbit_wrong_arity_2 test_rsetbit_rgetbit_wrong_arity_4" | redis-cli)
-  EXPECTED="2"
-  [ "$FOUND" == "$EXPECTED" ]
-  FOUND=$(echo "R.BITOP XOR test_rsetbit_rgetbit_wrong_arity_dest_2 test_rsetbit_rgetbit_wrong_arity_2 test_rsetbit_rgetbit_wrong_arity_4" | redis-cli)
-  EXPECTED="1"
-  [ "$FOUND" == "$EXPECTED" ]
-  # TODO test dest content 
+  function wrong_arity()
+  {
+    EXPECTED="ERR syntax error"
+    FOUND=$(echo "R.BITOP test_rsetbit_rgetbit_wrong_arity NOOP srckey1 srckey2" | redis-cli)
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+  function setup()
+  {
+    FOUND=$(echo "R.SETBIT test_rbitop_4 1 1" | redis-cli)
+    FOUND=$(echo "R.SETBIT test_rbitop_4 2 1" | redis-cli)
+    FOUND=$(echo "R.SETBIT test_rbitop_2 1 1" | redis-cli)
+  }
+
+  function and()
+  {
+    FOUND=$(echo "R.BITOP AND test_rbitop_dest_2 test_rbitop_2 test_rbitop_4" | redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R.GETBIT test_rbitop_dest_2 1" | redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+
+  function or()
+  {
+    FOUND=$(echo "R.BITOP OR test_rbitop_dest_6 test_rbitop_2 test_rbitop_4" | redis-cli)
+    EXPECTED="2"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R.GETBIT test_rbitop_dest_6 1" | redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R.GETBIT test_rbitop_dest_6 2" | redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+
+  function xor()
+  {
+    FOUND=$(echo "R.BITOP XOR test_rbitop_dest_4 test_rbitop_2 test_rbitop_4" | redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R.GETBIT test_rbitop_dest_4 2" | redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+
+  function not()
+  {
+    FOUND=$(echo "R.BITOP NOT test_rbitop_dest_3 test_rbitop_4" | redis-cli)
+    EXPECTED="2"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R.GETBIT test_rbitop_dest_3 0" | redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R.GETBIT test_rbitop_dest_3 1" | redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+
+  function collision()
+  {
+    FOUND=$(echo "R.BITOP AND collision test_rbitop_4 test_rbitop_2" | redis-cli)
+    FOUND=$(echo "R.BITOP OR collision test_rbitop_4 test_rbitop_2" | redis-cli)
+  }
+
+  setup
+  wrong_arity
+  and
+  or
+  xor
+  and
+  collision
 }
 function test_done()
 {
