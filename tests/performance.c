b8878d700d46d3fd2bb34345be0d1ce738775662
Antonio Viggiano <agfviggiano@gmail.com>
2017-06-07 01:03:55 -0300

    First comparison between SETBIT and R.SETBIT

--- tests/performance.c
+++ tests/performance.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 #include "hiredis.h"
 
-int main(int argc, char* argv[]) {
-
-  unsigned int j;
+redisContext* create_context() {
   redisContext* c;
-  redisReply* reply;
-  const char* hostname = (argc > 1) ? argv[1] : "127.0.0.1";
-  int port = (argc > 2) ? atoi(argv[2]) : 6379;
+  const char hostname[] = "127.0.0.1";
+  int port = 6379;
 
-  struct timeval timeout = {1, 500000}; // 1.5 seconds
+  struct timeval timeout = {2, 0};
   c = redisConnectWithTimeout(hostname, port, timeout);
   if (c == NULL || c->err) {
     if (c) {
@@ -22,56 +19,36 @@ int main(int argc, char* argv[]) {
     }
     exit(1);
   }
+  return c;
+}
 
-  /* PING server */
-  reply = redisCommand(c, "PING");
-  printf("PING: %s\n", reply->str);
-  freeReplyObject(reply);
+int main(int argc, char* argv[]) {
 
-  /* Set a key */
-  reply = redisCommand(c, "SET %s %s", "foo", "hello world");
-  printf("SET: %s\n", reply->str);
-  freeReplyObject(reply);
+  redisContext* c = create_context();
+  const char* ops[] = {
+      "R.SETBIT",
+      "SETBIT"
+  };
 
-  /* Set a key using binary safe API */
-  reply = redisCommand(c, "SET %b %b", "bar", (size_t) 3, "hello", (size_t) 5);
-  printf("SET (binary API): %s\n", reply->str);
-  freeReplyObject(reply);
-
-  /* Try a GET and two INCR */
-  reply = redisCommand(c, "GET foo");
-  printf("GET foo: %s\n", reply->str);
-  freeReplyObject(reply);
-
-  reply = redisCommand(c, "INCR counter");
-  printf("INCR counter: %lld\n", reply->integer);
-  freeReplyObject(reply);
-  /* again ... */
-  reply = redisCommand(c, "INCR counter");
-  printf("INCR counter: %lld\n", reply->integer);
-  freeReplyObject(reply);
-
-  /* Create a list of numbers, from 0 to 9 */
-  reply = redisCommand(c, "DEL mylist");
-  freeReplyObject(reply);
-  for (j = 0; j < 10; j++) {
-    char buf[64];
-
-    snprintf(buf, 64, "%u", j);
-    reply = redisCommand(c, "LPUSH mylist element-%s", buf);
-    freeReplyObject(reply);
-  }
-
-  /* Let's check what we have inside the list */
-  reply = redisCommand(c, "LRANGE mylist 0 -1");
-  if (reply->type == REDIS_REPLY_ARRAY) {
-    for (j = 0; j < reply->elements; j++) {
-      printf("%u) %s\n", j, reply->element[j]->str);
+  size_t N = 10000;
+  for (int op = 0; op < 2; op++) {
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    for (size_t i = 0; i < N; i++) {
+      redisReply* reply = redisCommand(c, "%s key%d %d", ops[op], op, i);
+      freeReplyObject(reply);
     }
-  }
-  freeReplyObject(reply);
+    clock_gettime(CLOCK_MONOTONIC, &end);
 
-  /* Disconnects and frees the context */
+    unsigned long ns = (end.tv_sec - start.tv_sec) * 1000000000UL + (end.tv_nsec - start.tv_nsec);
+    unsigned long us = ns / 1000;
+    unsigned long us_per_op = us / N;
+    printf("\n");
+    printf("Operation: %s\n", ops[op]);
+    printf("Total time: %lu us\n", us);
+    printf("Time/op: %lu us\n", us_per_op);
+    printf("\n");
+  }
   redisFree(c);
 
   return 0;
