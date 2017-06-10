871924754c76051516cdd9eeb2a3866c08f71ad6
Antonio Viggiano <agfviggiano@gmail.com>
2017-06-10 18:58:10 -0300

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,16 @@ set -ex
 SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
-function update_gh_pages {
-  cat "/tmp/test.out" > docs/README.md
+function update_readme {
+  sed -i "/## Performance/q" docs/README.md
+  local BUILD_URL="https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+  echo "Tested using CRoaring's \`census1881\` dataset on [travis]($BUILD_URL):" >> docs/README.md
+  echo "" >> docs/README.md
+  cat "/tmp/test.out" >> docs/README.md
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" -o "$TRAVIS_TAG" != "deploy" ]; then
     echo "Skipping deploy."
     exit 0
 fi
@@ -21,32 +25,13 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into docs/
-# Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO docs
-cd docs
-git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
-cd ..
-
-# Clean docs existing contents
-rm -rf docs/**/* || exit 0
-
-# Run our compile script
-update_gh_pages
-
 # Now let's go have some fun with the cloned repo
-cd docs
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled docs (e.g. this is a README update) then just bail.
-if git diff --quiet; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
+update_readme
 git add -A .
 git commit -m "Deploy to GitHub Pages: ${SHA}"
 
@@ -55,10 +40,10 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
-chmod 600 ../deploy_key
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
 eval `ssh-agent -s`
-ssh-add ../deploy_key
+ssh-add deploy_key
 
 # Now that we're all set up, we can push.
 git push $SSH_REPO $TARGET_BRANCH
