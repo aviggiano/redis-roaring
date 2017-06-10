ad40c6eaf264a81b57117d86eccf1557eb4ed0d4
Antonio Viggiano <agfviggiano@gmail.com>
2017-06-10 13:18:42 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function update_gh_pages {
-  echo "Update gh-pages :)" >> out/README.md
+  echo "Update gh-pages :)" >> docs/README.md
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -22,25 +22,25 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing gh-pages for this repo into out/
+# Clone the existing gh-pages for this repo into docs/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO docs
+cd docs
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
-# Clean out existing contents
-rm -rf out/**/* || exit 0
+# Clean docs existing contents
+rm -rf docs/**/* || exit 0
 
 # Run our compile script
 update_gh_pages
 
 # Now let's go have some fun with the cloned repo
-cd out
+cd docs
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+# If there are no changes to the compiled docs (e.g. this is a README update) then just bail.
 if git diff --quiet; then
     echo "No changes to the output on this push; exiting."
     exit 0
@@ -56,7 +56,7 @@ ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
 ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -out ../deploy_key -d
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in ../deploy_key.enc -docs ../deploy_key -d
 chmod 600 ../deploy_key
 eval `ssh-agent -s`
 ssh-add deploy_key
