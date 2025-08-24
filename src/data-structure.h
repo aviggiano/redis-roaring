06e536f4e5eec4c3afd218338f2e9bbbcc060fbf
Andrew L <andrew.io.dev@gmail.com>
2025-08-24 16:32:01 +0200

    add support for 64-bit #110

--- src/data-structure.h
+++ src/data-structure.h
@@ -6,11 +6,19 @@
 typedef roaring_bitmap_t Bitmap;
 typedef roaring_statistics_t Bitmap_statistics;
 
+typedef roaring64_bitmap_t Bitmap64;
+typedef roaring64_statistics_t Bitmap64_statistics;
+
 Bitmap* bitmap_alloc();
+Bitmap64* bitmap64_alloc();
 void bitmap_free(Bitmap* bitmap);
+void bitmap64_free(Bitmap64* bitmap);
 uint64_t bitmap_get_cardinality(const Bitmap* bitmap);
+uint64_t bitmap64_get_cardinality(const Bitmap64* bitmap);
 void bitmap_setbit(Bitmap* bitmap, uint32_t offset, bool value);
+void bitmap64_setbit(Bitmap64* bitmap, uint64_t offset, bool value);
 bool bitmap_getbit(const Bitmap* bitmap, uint32_t offset);
+bool bitmap64_getbit(const Bitmap64* bitmap, uint64_t offset);
 /**
  * Gets the n-th element of the set.
  *
@@ -22,6 +30,7 @@ bool bitmap_getbit(const Bitmap* bitmap, uint32_t offset);
  * @example set {1, 10, 100, 1000} and n=1 returns 1, n=3 returns 100, n=0 returns -1, n=5 returns -1
  */
 int64_t bitmap_get_nth_element_present(const Bitmap* bitmap, uint64_t n);
+int64_t bitmap64_get_nth_element_present(const Bitmap64* bitmap, uint64_t n);
 /**
  * Gets the n-th element not present in the set
  *
@@ -33,14 +42,19 @@ int64_t bitmap_get_nth_element_present(const Bitmap* bitmap, uint64_t n);
  * @example set {1, 2, 4, 5, 7} and n=1 returns 0, n=2 returns 3, n=3 returns 6, n=5 returns 9
  */
 int64_t bitmap_get_nth_element_not_present(const Bitmap* bitmap, uint64_t n);
+int64_t bitmap64_get_nth_element_not_present(const Bitmap64* bitmap, uint64_t n);
 /**
  * Same as `bitmap_get_nth_element_not_present` but slower. Useful for cross validation.
  */
 int64_t bitmap_get_nth_element_not_present_slow(const Bitmap* bitmap, uint64_t n);
 Bitmap* bitmap_or(uint32_t n, const Bitmap** bitmaps);
+Bitmap64* bitmap64_or(uint32_t n, const Bitmap64** bitmaps);
 Bitmap* bitmap_and(uint32_t n, const Bitmap** bitmaps);
+Bitmap64* bitmap64_and(uint32_t n, const Bitmap64** bitmaps);
 Bitmap* bitmap_xor(uint32_t n, const Bitmap** bitmaps);
+Bitmap64* bitmap64_xor(uint32_t n, const Bitmap64** bitmaps);
 Bitmap* bitmap_flip(const Bitmap* bitmap, uint32_t end); 
+Bitmap64* bitmap64_flip(const Bitmap64* bitmap, uint64_t end); 
 /**
  * Returns a bitmap equals to the inverted set of first bitmap of the `bitmaps` array,
  * ignoring the first parameter of the function.  * The purpose of this function is to
@@ -54,9 +68,13 @@ Bitmap* bitmap_flip(const Bitmap* bitmap, uint32_t end);
 Bitmap* bitmap_not_array(uint32_t unused, const Bitmap** bitmaps);
 Bitmap* bitmap_not(const Bitmap* bitmap);
 Bitmap* bitmap_from_int_array(size_t n, const uint32_t* array);
+Bitmap64* bitmap64_from_int_array(size_t n, const uint64_t* array);
 uint32_t* bitmap_get_int_array(const Bitmap* bitmap, size_t* n);
+uint64_t* bitmap64_get_int_array(const Bitmap64* bitmap, size_t* n);
 uint32_t* bitmap_range_int_array(const Bitmap* bitmap, size_t offset, size_t n);
+uint64_t* bitmap64_range_int_array(const Bitmap64* bitmap, size_t offset, size_t n);
 void bitmap_free_int_array(uint32_t* array);
+void bitmap64_free_int_array(uint64_t* array);
 /**
  * Creates a Bitmap from a buffer composed from ASCII '0's and '1's
  *
@@ -65,6 +83,7 @@ void bitmap_free_int_array(uint32_t* array);
  * @return a Bitmap corresponding to the bit array
  */
 Bitmap* bitmap_from_bit_array(size_t size, const char* array);
+Bitmap64* bitmap64_from_bit_array(size_t size, const char* array);
 /**
  * Creates a buffer string composed from ASCII '0's and '1's from a Bitmap
  *
@@ -73,6 +92,7 @@ Bitmap* bitmap_from_bit_array(size_t size, const char* array);
  * @return the buffer bit array
  */
 char* bitmap_get_bit_array(const Bitmap* bitmap, size_t* size);
+char* bitmap64_get_bit_array(const Bitmap64* bitmap, uint64_t* size);
 void bitmap_free_bit_array(char* array);
 /**
  * Creates a roaring bitmap filled with a range of numbers
@@ -82,10 +102,16 @@ void bitmap_free_bit_array(char* array);
  * @return the roaring bitmap
  */
 Bitmap* bitmap_from_range(uint64_t from, uint64_t to);
+Bitmap64* bitmap64_from_range(uint64_t from, uint64_t to);
 bool bitmap_is_empty(const Bitmap* bitmap);
+bool bitmap64_is_empty(const Bitmap64* bitmap);
 uint32_t bitmap_min(const Bitmap* bitmap);
+uint64_t bitmap64_min(const Bitmap64* bitmap);
 uint32_t bitmap_max(const Bitmap* bitmap);
+uint64_t bitmap64_max(const Bitmap64* bitmap);
 void bitmap_optimize(Bitmap* bitmap, int shrink_to_fit);
+void bitmap64_optimize(Bitmap64* bitmap, int shrink_to_fit);
 void bitmap_statistics(const Bitmap *bitmap, Bitmap_statistics *stat);
+void bitmap64_statistics(const Bitmap64 *bitmap, Bitmap64_statistics *stat);
 
 #endif
---
+++ tests/integration_3.sh
@@ -0,0 +1,352 @@
+#!/usr/bin/env bash
+
+set -eu
+
+function test_setbit_getbit()
+{
+  echo "test_setbit_getbit"
+
+  EXPECTED="0"
+  FOUND=$(echo "R64.SETBIT test_setbit_getbit 0 1" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+  EXPECTED="1"
+  FOUND=$(echo "R64.GETBIT test_setbit_getbit 0" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+
+  EXPECTED="ERR wrong number of arguments for 'R64.SETBIT' command"
+  FOUND=$(echo "R64.SETBIT test_setbit_getbit 0" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+  EXPECTED="ERR wrong number of arguments for 'R64.GETBIT' command"
+  FOUND=$(echo "R64.GETBIT key 0 1" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_bitop()
+{
+  echo "test_bitop"
+  function wrong_arity()
+  {
+    EXPECTED="ERR syntax error"
+    FOUND=$(echo "R64.BITOP test_bitop_wrong_arity NOOP srckey1 srckey2" | ./deps/redis/src/redis-cli)
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+  function setup()
+  {
+    FOUND=$(echo "R64.SETBIT test_bitop_4 1 1" | ./deps/redis/src/redis-cli)
+    FOUND=$(echo "R64.SETBIT test_bitop_4 2 1" | ./deps/redis/src/redis-cli)
+    FOUND=$(echo "R64.SETBIT test_bitop_2 1 1" | ./deps/redis/src/redis-cli)
+  }
+
+  function and()
+  {
+    FOUND=$(echo "R64.BITOP AND test_bitop_dest_2 test_bitop_2 test_bitop_4" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R64.GETBIT test_bitop_dest_2 1" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+
+  function or()
+  {
+    FOUND=$(echo "R64.BITOP OR test_bitop_dest_6 test_bitop_2 test_bitop_4" | ./deps/redis/src/redis-cli)
+    EXPECTED="2"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R64.GETBIT test_bitop_dest_6 1" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R64.GETBIT test_bitop_dest_6 2" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+
+  function xor()
+  {
+    FOUND=$(echo "R64.BITOP XOR test_bitop_dest_4 test_bitop_2 test_bitop_4" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R64.GETBIT test_bitop_dest_4 2" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+
+  function not()
+  {
+    FOUND=$(echo "R64.BITOP NOT test_bitop_dest_3 test_bitop_4" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R64.GETBIT test_bitop_dest_3 0" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R64.MAX test_bitop_dest_3" | ./deps/redis/src/redis-cli)
+    EXPECTED="0"
+    [ "$FOUND" == "$EXPECTED" ]
+
+    FOUND=$(echo "R64.BITOP NOT test_bitop_dest_3 test_bitop_4 3" | ./deps/redis/src/redis-cli)
+    EXPECTED="2"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R64.GETBIT test_bitop_dest_3 0" | ./deps/redis/src/redis-cli)
+    EXPECTED="1"
+    [ "$FOUND" == "$EXPECTED" ]
+    FOUND=$(echo "R64.MAX test_bitop_dest_3" | ./deps/redis/src/redis-cli)
+    EXPECTED="3"
+    [ "$FOUND" == "$EXPECTED" ]
+  }
+
+  function collision()
+  {
+    FOUND=$(echo "R64.BITOP AND test_bitop_collision test_bitop_4 test_bitop_2" | ./deps/redis/src/redis-cli)
+    FOUND=$(echo "R64.BITOP OR test_bitop_collision test_bitop_4 test_bitop_2" | ./deps/redis/src/redis-cli)
+  }
+
+  setup
+  wrong_arity
+  and
+  or
+  xor
+  and
+  collision
+}
+function test_bitcount()
+{
+  echo "test_bitcount"
+  FOUND=$(echo "R64.SETBIT test_bitcount 1 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitcount 1 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitcount 2 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitcount 3 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitcount 5 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitcount 8 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitcount 13 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.BITCOUNT test_bitcount" | ./deps/redis/src/redis-cli)
+  EXPECTED="6"
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_bitpos()
+{
+  echo "test_bitpos"
+  FOUND=$(echo "R64.SETBIT test_bitpos_1 3 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_1 6 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_1 4 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_1 12 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_1 10 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.BITPOS test_bitpos_1 1" | ./deps/redis/src/redis-cli)
+  EXPECTED="3"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.SETBIT test_bitpos_0 0 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_0 1 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_0 2 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_0 3 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_0 4 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETBIT test_bitpos_0 6 1" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.BITPOS test_bitpos_0 0" | ./deps/redis/src/redis-cli)
+  EXPECTED="5"
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_getintarray_setintarray()
+{
+  echo "test_getintarray_setintarray"
+
+  FOUND=$(echo "R64.SETINTARRAY test_getintarray_setintarray 0" | ./deps/redis/src/redis-cli)
+  EXPECTED="OK"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETINTARRAY test_getintarray_setintarray" | ./deps/redis/src/redis-cli)
+  EXPECTED="0"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.SETINTARRAY test_getintarray_setintarray 1 10 100 1000 10000 100000" | ./deps/redis/src/redis-cli)
+  EXPECTED="OK"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETINTARRAY test_getintarray_setintarray" | ./deps/redis/src/redis-cli)
+  EXPECTED=$(echo -e "1\n10\n100\n1000\n10000\n100000")
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.SETINTARRAY test_getintarray_setintarray 2 4 8 16 32 64 128" | ./deps/redis/src/redis-cli)
+  EXPECTED="OK"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETINTARRAY test_getintarray_setintarray" | ./deps/redis/src/redis-cli)
+  EXPECTED=$(echo -e "2\n4\n8\n16\n32\n64\n128")
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.GETINTARRAY test_getintarray_setintarray_empty_key" | ./deps/redis/src/redis-cli)
+  EXPECTED=""
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_getbitarray_setbitarray()
+{
+  echo "test_getbitarray_setbitarray"
+
+  FOUND=$(echo "R64.SETBITARRAY test_getbitarray_setbitarray 0" | ./deps/redis/src/redis-cli)
+  EXPECTED="OK"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETBITARRAY test_getbitarray_setbitarray" | ./deps/redis/src/redis-cli)
+  EXPECTED="0"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.SETBITARRAY test_getbitarray_setbitarray 0110100100010010011001000" | ./deps/redis/src/redis-cli)
+  EXPECTED="OK"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETBITARRAY test_getbitarray_setbitarray" | ./deps/redis/src/redis-cli)
+  EXPECTED="0110100100010010011001"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETBIT test_getbitarray_setbitarray 0" | ./deps/redis/src/redis-cli)
+  EXPECTED="0"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETBIT test_getbitarray_setbitarray 1" | ./deps/redis/src/redis-cli)
+  EXPECTED="1"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETBIT test_getbitarray_setbitarray 24" | ./deps/redis/src/redis-cli)
+  EXPECTED="0"
+  [ "$FOUND" == "$EXPECTED" ]
+  FOUND=$(echo "R64.GETBIT test_getbitarray_setbitarray 21" | ./deps/redis/src/redis-cli)
+  EXPECTED="1"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.GETBITARRAY test_getbitarray_setbitarray_empty_key" | ./deps/redis/src/redis-cli)
+  EXPECTED=""
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_min_max()
+{
+  echo "test_min_max"
+
+  FOUND=$(echo "R64.MIN test_min_max" | ./deps/redis/src/redis-cli)
+  EXPECTED="-1"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.MAX test_min_max" | ./deps/redis/src/redis-cli)
+  EXPECTED="-1"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  echo "R64.SETBIT test_min_max 100 1" | ./deps/redis/src/redis-cli
+
+  FOUND=$(echo "R64.MIN test_min_max" | ./deps/redis/src/redis-cli)
+  EXPECTED="100"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.MAX test_min_max" | ./deps/redis/src/redis-cli)
+  EXPECTED="100"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  echo "R64.SETBIT test_min_max 0 1" | ./deps/redis/src/redis-cli
+
+  FOUND=$(echo "R64.MIN test_min_max" | ./deps/redis/src/redis-cli)
+  EXPECTED="0"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.MAX test_min_max" | ./deps/redis/src/redis-cli)
+  EXPECTED="100"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.SETBIT test_min_max 0 0" | ./deps/redis/src/redis-cli)
+  EXPECTED="1"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.SETBIT test_min_max 100 0" | ./deps/redis/src/redis-cli)
+   EXPECTED="1"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.MIN test_min_max" | ./deps/redis/src/redis-cli)
+  EXPECTED="-1"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.MAX test_min_max" | ./deps/redis/src/redis-cli)
+  EXPECTED="-1"
+  [ "$FOUND" == "$EXPECTED" ]
+
+}
+function test_diff()
+{
+  echo "test_diff"
+
+  EXPECTED="ERR wrong number of arguments for 'R64.DIFF' command"
+  FOUND=$(echo "R64.DIFF test_diff_res" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+
+  EXPECTED="WRONGTYPE Operation against a key holding the wrong kind of value"
+  FOUND=$(echo "R64.DIFF test_diff_res empty_key_1 empty_key_2" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.SETINTARRAY test_diff_1 1 2 3 4" | ./deps/redis/src/redis-cli)
+  FOUND=$(echo "R64.SETINTARRAY test_diff_2 3 4 5 6" | ./deps/redis/src/redis-cli)
+
+  EXPECTED="OK"
+  FOUND=$(echo "R64.DIFF test_diff_res test_diff_1 test_diff_2" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+ 
+  FOUND=$(echo "R64.GETINTARRAY test_diff_res" | ./deps/redis/src/redis-cli)
+  EXPECTED=$(echo -e "1\n2")
+  [ "$FOUND" == "$EXPECTED" ]
+
+  EXPECTED="OK"
+  FOUND=$(echo "R64.DIFF test_diff_res test_diff_2 test_diff_1" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+ 
+  FOUND=$(echo "R64.GETINTARRAY test_diff_res" | ./deps/redis/src/redis-cli)
+  EXPECTED=$(echo -e "5\n6")
+  echo $FOUND;
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_optimize_nokey()
+{
+  echo "test_optimize nokey"
+  FOUND=$(echo "R64.OPTIMIZE no-key" | ./deps/redis/src/redis-cli)
+  EXPECTED="ERR no such key"
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_setfull()
+{
+  echo "test_setfull"
+  echo "del foo" | ./deps/redis/src/redis-cli
+  echo "R64.SETFULL foo" | ./deps/redis/src/redis-cli
+  FOUND=$(echo "R64.BITCOUNT foo" | ./deps/redis/src/redis-cli)
+  EXPECTED="4294967296"
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_del()
+{
+  echo "test_del"
+
+  FOUND=$(echo "R64.SETBIT test_del 0 1" | ./deps/redis/src/redis-cli)
+  EXPECTED="0"
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "DEL test_del" | ./deps/redis/src/redis-cli)
+  EXPECTED="1"
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_stat()
+{
+  echo "test_stat"
+
+  EXPECTED="0"
+  FOUND=$(echo "R64.SETBIT test_stat 100 1" | ./deps/redis/src/redis-cli)
+  [ "$FOUND" == "$EXPECTED" ]
+
+  FOUND=$(echo "R64.STAT test_stat" | ./deps/redis/src/redis-cli)
+  EXPECTED=$'cardinality: 1\nnumber of containers: 1\nmax value: 100\nmin value: 100
+number of array containers: 1\n\tarray container values: 1\n\tarray container bytes: 2
+bitset  containers: 0\n\tbitset  container values: 0\n\tbitset  container bytes: 0
+run containers: 0\n\trun container values: 0\n\trun container bytes: 0'
+  [ "$FOUND" == "$EXPECTED" ]
+}
+function test_save()
+{
+  echo "test_save"
+
+  FOUND=$(echo "SAVE" | ./deps/redis/src/redis-cli)
+  EXPECTED="OK"
+}
+
+test_setbit_getbit
+test_bitop
+test_bitcount
+test_bitpos
+test_getintarray_setintarray
+test_getbitarray_setbitarray
+test_min_max
+test_diff
+test_optimize_nokey
+# test_setfull
+test_del
+test_stat
+test_save
